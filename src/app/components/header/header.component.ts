8147df2e05b6d98c91e9b3443b628e2132c507c5
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-22 19:40:58 -0300

    .

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../menu/menu.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule, CommonModule, MenuComponent, RouterOutlet],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isMenuCollapsed: boolean = true;
+
+  onMenuToggle(state: boolean) {
+    this.isMenuCollapsed = state;
+  }
+}
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {
+  @Input({ required: true }) isCollapsed!: boolean;
+  @Output() changeIsCollapsed = new EventEmitter<boolean>();
+
+  menuItems = [
+    { routeLink: 'home', label: 'Home' },
+    { routeLink: 'dashboard', label: 'Dashboard' },
+  ];
+
+  toggleCollapse(): void {
+    this.changeIsCollapsed.emit(!this.isCollapsed);
+  }
+
+  closeMenu(): void {
+    this.changeIsCollapsed.emit(true);
+  }
+}

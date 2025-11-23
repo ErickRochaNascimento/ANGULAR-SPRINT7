0ec50c5dacfd880c71ff295b1b2784f9b6d189fd
Erick Rocha Nascimento <erickrochaluciano@gmail.com>
2025-11-23 00:30:06 -0300

    Menu sem atrapalhar o conteudo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,36 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path:'login', component:LoginComponent},
-    {path:'home', component:HomeComponent},
-    {path:'dashboard', component:DashboardComponent},
-    {path:'',redirectTo:'login',pathMatch:"full"}
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        loadComponent: () => import('./components/header/header.component').then(m => m.HeaderComponent),
+        children: [
+            {
+                path: 'home',
+                loadComponent: () =>
+                    import('./pages/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'dashboard',
+                loadComponent: () =>
+                    import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+            },
+            // Redirecionar a rota vazia do header para home
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            }
+        ]
+    },
 ];
